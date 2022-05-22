dd1ea71e378a285f82912ff08256e7baeb2921fa
Daniel Courtney <dan4310@gmail.com>
2022-05-22 00:17:30 -0400

    feat(footer): better footer

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,5 @@
+var isNavActive = false;
+
 window.onload = function () {
     var navbar = document.getElementById('navbar')
     window.onscroll = function() {
@@ -6,5 +8,28 @@ window.onload = function () {
             navbar.classList.add('scroll')
         else
             navbar.classList.remove('scroll')
+    }
+
+    var btnnav = document.getElementById('btn-nav')
+    btnnav.onclick = function() {
+        isNavActive = !isNavActive;
+        var sidebar = document.getElementById('nav-sidebar')
+        if (isNavActive) {
+            sidebar.classList.add('nav-active')
+            sidebar.classList.remove('nav-inactive')
+        }  
+        else {
+            sidebar.classList.remove('nav-active')
+            sidebar.classList.add('nav-inactive')
+        }
+    }
+
+    var btnemail = document.getElementById('btn-email')
+    btnemail.onclick = function() {
+        var email = 'dan4310@gmail.com'
+        var subject = 'Some subject'.replace(' ', '%20')
+        var body = 'message goes here'.replace(' ', '%20')
+
+        window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
     }
 }
